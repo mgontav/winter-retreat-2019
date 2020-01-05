05a12c6f4e4a973a8f774bd610a5839b21f5d413
Pedro Janeiro <pjaneiro@whitesmith.co>
2020-01-05 05:16:31 +0000

    Customize active admin for people

---
+++ app/admin/people.rb
@@ -0,0 +1,35 @@
+# frozen_string_literal: true
+
+ActiveAdmin.register Person do
+  actions :index, :show, :edit, :update
+
+  permit_params :name, :team_id
+
+  index do
+    selectable_column
+
+    column :name
+    column :team
+
+    actions
+  end
+
+  show do
+    attributes_table do
+      row :id
+      row :name
+      row :team
+    end
+  end
+
+  form do |f|
+    f.semantic_errors(*f.object.errors.keys)
+
+    f.inputs do
+      f.input :name
+      f.input :team
+    end
+
+    f.actions
+  end
+end
